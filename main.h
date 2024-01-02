e68c53e7b8d02a4c4cd26a58e47c74d41c892f81
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-01-03 01:24:10 +0300

    printf

--- main.h
+++ main.h
@@ -20,6 +20,13 @@
 #define S_SHORT 1
 
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 #endif // MAIN_H
