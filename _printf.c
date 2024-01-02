928870060ac71253cc2ab2d8950d00af56932815
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-01-03 01:27:00 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -3,36 +3,35 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints a formatted string
- * @format: string to be printed
- *
- * Return: number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_index = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(list, format);
+	va_start(list, format);
 
-    for (i = 0; format && format[i] !='\0': i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_index++] = format[i];
-            if (buff_index == BUFF_SIZE)
-                print_buffer(buffer, &buff_index);
-            /**/
-            printed_chars++;
-        }
-        else
-        {
-            print_buffer(buffer, &buff_ind);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
@@ -43,9 +42,10 @@ int _printf(const char *format, ...)
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
-        }
-    }
-    print_buffer(buffer, &buff_ind);
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
